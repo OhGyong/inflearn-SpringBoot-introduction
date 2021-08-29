8564843b70914116e85e996d9b3a32f91e963638
OhGyong <oky7143@naver.com>
2021-08-29 22:35:46 +0900

    인프런 스프링 부트 강의

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,52 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+// 핵심 비즈니스 로직 구현
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 같은 이름이 있는 회원 있으면 에러처리
+    private void validateDuplicateMember(Member member) {
+        // if 문 대신 Optional의 메서드 ifPresent 사용
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // result의 값이 null이 아니면(값이 존재하면)
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * id 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}

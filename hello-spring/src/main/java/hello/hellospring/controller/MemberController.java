9dc8f1885e5940dc3ee46ce2a8b751402af9668b
OhGyong <oky7143@naver.com>
2021-08-30 22:11:08 +0900

    회원 웹 기능 - 조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,11 +20,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    /**
+     * function: 회원 등록 페이지 이동
+     */
     @GetMapping("/members/new")
     public String createForm() {
         return "members/memberCreateForm";
     }
 
+
+    /**
+     * function: 회원 등록
+     */
     @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
@@ -31,4 +41,16 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    /**
+     * function:회원 목록 페이지로 이동
+     */
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // 멤버의 리스트 자체를 모델에 담아서 화면에 넘길것
+        return "members/memberList";
+    }
+
+
 }
